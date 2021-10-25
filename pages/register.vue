a2696c52c452fbeda84a3ce36e74f8ee0e548653
ykikuchi <y.kikuchi5101@gmail.com>
2021-10-25 23:02:45 +0900

    スペスミスを修正

--- pages/register.vue
+++ pages/register.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="register">
-    <label>メールアドレス：<input v-model="email" type="email" required/></label>
-    <br>
-    <label>パスワード： <input v-model="password" type="password" required/></label>
+    <label
+      >メールアドレス： <input v-model="email" type="email" required
+    /></label>
+    <br />
+    <label
+      >パスワード： <input v-model="password" type="password" required
+    /></label>
     <br />
     <button @click="register">新規登録</button>
     <br />
@@ -13,42 +17,42 @@
 <script>
 import firebase from '~/plugins/firebase'
 export default {
-  data(){
-    return{
-      email:null,
-      password:null,
+  data() {
+    return {
+      email: null,
+      password: null,
     }
   },
-  methods:{
-    register(){
-      if(!this.email || !this.password){
+  methods: {
+    register() {
+      if (!this.email || !this.password) {
         alert('メールアドレスまたはパスワードが入力されていません。')
         return
       }
       firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.email, this.password)
-      .then((data)=>{
-        data.user.sendEmailVerificaion().then(()=>{
-          this.$router.replace('/confirm')
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then((data) => {
+          data.user.sendEmailVerification().then(() => {
+            this.$router.replace('/confirm')
+          })
         })
-      })
-      .catch((error)=>{
-        switch(error.code){
-          case 'auth/invalid-email':
-            alert('メールアドレスが違います')
-            break
-          case 'auth/email-already-in-use':
-            alert('このメードアドレスはすでに使われています。')
-            break
-          case 'auth/weak-password':
-            alert('パスワードは６文字以上で入力してください')
-            break
-          default:
-            alert('エラーが発生しました。しばらくしてから再度お試しください。')
-            break
-        }
-      })
+        .catch((error) => {
+          switch (error.code) {
+            case 'auth/invalid-email':
+              alert('メールアドレスの形式が違います。')
+              break
+            case 'auth/email-already-in-use':
+              alert('このメールアドレスはすでに使われています。')
+              break
+            case 'auth/weak-password':
+              alert('パスワードは6文字以上で入力してください。')
+              break
+            default:
+              alert('エラーが起きました。しばらくしてから再度お試しください。')
+              break
+          }
+        })
     },
   },
 }
